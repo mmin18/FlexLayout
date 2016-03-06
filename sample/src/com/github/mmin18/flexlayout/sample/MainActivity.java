73539038f166923740cf4cfaaeb32f58fcc01482
Tu Yimin <mmin18@gmail.com>
2016-03-06 17:42:32 +0800

    layout sample files

--- sample/src/com/github/mmin18/flexlayout/sample/MainActivity.java
+++ sample/src/com/github/mmin18/flexlayout/sample/MainActivity.java
@@ -1,7 +1,14 @@
 package com.github.mmin18.flexlayout.sample;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 /**
  * Created by mmin18 on 3/5/16.
@@ -10,8 +17,34 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.flex_layout_x);
+		setContentView(R.layout.flex_layout);
 
-		findViewById(android.R.id.text1).setTag(12);
+		setupCatalogViews();
+
+//		findViewById(android.R.id.text1).setTag(12);
+	}
+
+	private void setupCatalogViews() {
+		ViewGroup frame = (ViewGroup) findViewById(R.id.frame);
+		frame.removeAllViews();
+		LayoutInflater inflater = getLayoutInflater();
+
+		int i = R.layout.flex_layout & 0xffff0000;
+		Pattern pattern = Pattern.compile("sample\\d+[\\d\\w_]*");
+		try {
+			while (true) {
+				String s = getResources().getResourceEntryName(i);
+				if (pattern.matcher(s).matches()) {
+					TextView tv = (TextView) inflater.inflate(R.layout.catalog_header, frame, false);
+					tv.setText("@layout/" + s);
+					frame.addView(tv);
+					inflater.inflate(i, frame, true);
+				}
+				i++;
+			}
+		} catch (Resources.NotFoundException e) {
+		} catch (Exception e) {
+			Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
+		}
 	}
 }
